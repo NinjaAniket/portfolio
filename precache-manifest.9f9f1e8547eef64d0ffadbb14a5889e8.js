dd705cd654bea122d08f01970f32c638a9bde3c2
Admin <admin@Admins-MacBook-Air.local>
2020-10-05 20:41:24 +0530

    Updates

---
+++ precache-manifest.9f9f1e8547eef64d0ffadbb14a5889e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e85251bb07080355d5cd54700090cc",
+    "url": "/NinjaAniket/portfolio/index.html"
+  },
+  {
+    "revision": "e0b8bfa5fe66d44077c4",
+    "url": "/NinjaAniket/portfolio/static/css/main.4203a644.chunk.css"
+  },
+  {
+    "revision": "3a10c3f3e985e627315d",
+    "url": "/NinjaAniket/portfolio/static/js/2.5886da71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NinjaAniket/portfolio/static/js/2.5886da71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b8bfa5fe66d44077c4",
+    "url": "/NinjaAniket/portfolio/static/js/main.61d72f08.chunk.js"
+  },
+  {
+    "revision": "f3c7ff483bdc6d81049a",
+    "url": "/NinjaAniket/portfolio/static/js/runtime-main.4bac587d.js"
+  },
+  {
+    "revision": "abbf56e9c7e7b0ee2b19924549225947",
+    "url": "/NinjaAniket/portfolio/static/media/design.abbf56e9.jpg"
+  },
+  {
+    "revision": "680cadf3dfadb5b2a5fbb26d2b750bc2",
+    "url": "/NinjaAniket/portfolio/static/media/pr-1.680cadf3.png"
+  },
+  {
+    "revision": "251781c924d6c29935c16621f45a792a",
+    "url": "/NinjaAniket/portfolio/static/media/pr-2.251781c9.png"
+  },
+  {
+    "revision": "4277a155f071b80a8b74a845edccc9f6",
+    "url": "/NinjaAniket/portfolio/static/media/pr-3.4277a155.png"
+  },
+  {
+    "revision": "febe4a750a86d9dcbd11552058154afd",
+    "url": "/NinjaAniket/portfolio/static/media/pr-4.febe4a75.png"
+  }
+]);
