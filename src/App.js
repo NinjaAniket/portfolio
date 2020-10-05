c626561db013142b94a0b37ac18977f7c67e5b17
aniket <aniketvnagapure@gmail.com>
2020-10-05 22:35:08 +0530

    scss order changed in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import Header from "./components/header/Header";
+
 import "./App.scss";
+
+import Header from "./components/header/Header";
 import Banner from "./components/banner/Banner";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
